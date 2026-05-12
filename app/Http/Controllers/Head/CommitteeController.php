184867c12a46f0e39faadcbb82b9cf4deb226c7b
Hidayah <hidayah_burhannudin@uptm.edu.my>
2026-05-12 11:03:34 +0800

    change capitalize

--- app/Http/Controllers/Head/CommitteeController.php
+++ app/Http/Controllers/Head/CommitteeController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Program;
 use App\Models\Staff;
-use App\Models\ProgramStaff;
+use App\Models\Programstaff;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
