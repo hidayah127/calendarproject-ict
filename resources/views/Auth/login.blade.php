e4d5e8c496ba963728ae3beda5afb33f906356a9
wlvshrt <dayah.ficts@gmail.com>
2026-08-05 17:14:42 +0800

    rename

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -383,7 +383,7 @@ body {
 
     <p class="login-footer">
         &copy; {{ date('Y') }} AmazingTrack — All rights reserved<br>
-        Developed by Information & Communication Technology Department UPTM
+        Developed by Information & Communication Technology Department UPTM.
     </p>
 
 </div>
